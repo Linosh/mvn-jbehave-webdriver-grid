9f59a53d89f5c451acb04cb65e15633c08e4a36e
Linosh <linoshmail@gmail.com>
2014-05-13 09:39:12 +0300

    Test script to read properties from command line

--- src/test/resources/grid/grid.sh
+++ src/test/resources/grid/grid.sh
@@ -1,5 +1,44 @@
 #!/bin/sh
 
+############ Working with props
+
+OPTIONS=$(getopt -o hf:gb -l help,file:,foo,bar -- "$@")
+
+if [ $? -ne 0 ]; then
+  echo "getopt error"
+  exit 1
+fi
+
+eval set -- $OPTIONS
+
+while true; do
+  case "$1" in
+    -h|--help) HELP="$2" ; shift ;;
+    -f|--file) FILE="$2" ; shift ;;
+    -g|--foo)  FOO="$2" ; shift ;;
+    -b|--bar)  BAR="$2" ; shift ;;
+    --)        shift ; break ;;
+    *)         echo "unknown option: $1" ; exit 1 ;;
+  esac
+  shift
+done
+
+if [ $# -ne 0 ]; then
+  echo "unknown option(s): $@"
+  exit 1
+fi
+
+echo "help: $HELP"
+echo "file: $FILE"
+echo "foo: $FOO"
+echo "bar: $BAR"
+
+
+
+###############################
+
+
+
 source grid.properties
 
 case "$1" in
